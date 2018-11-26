a0b32cf9b90e75c29710211b94b547d88d310ef5
Simon Balean <simonbalean@gmail.com>
2018-11-26 22:53:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |x, index|
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.collect do |x|
+    new_array "#{x.capitalize}!"
+    
+  end
 end
 
 def long_planeteer_calls# code an argument here
